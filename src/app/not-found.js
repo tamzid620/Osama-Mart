c751f7846229edeb8c148808d42323cca470cabe
tamzid620 <tamzid620@gmail.com>
2024-12-10 15:31:10 +0600

    not found page designed

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,11 +1,25 @@
 import Link from 'next/link'
- 
+import Image from 'next/image'
+import errorImage from '@/assests/images/404.png'
+import { Kanit } from "next/font/google";
+
+const kanit = Kanit({
+  weight: ["400", "700"],
+  style: ["normal"],
+});
+
 export default function NotFound() {
   return (
-    <div className='mt-20'>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
+    <div className='w-full h-screen'>
+      <div className='flex justify-center items-center'>
+      <Image className='max-w-3xl' src={errorImage} alt=''/>
+      </div>
+      <Link href="/" className='flex justify-center mt-10'>
+        <button 
+        className={` ${kanit.className}  bg-transparent hover:bg-[#F26626]
+         border-2 border-[#F26626] hover:border-white
+         px-4 py-2 rounded-sm`}> Back To Home</button>
+      </Link >
     </div>
   )
 }
