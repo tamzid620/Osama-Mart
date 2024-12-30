6c455df6619cfeffc4941b7dfd5f8d906687cd1c
tamzid620 <tamzid620@gmail.com>
2024-12-30 21:19:07 +0600

    api link changed1

--- src/utilies/config.js
+++ src/utilies/config.js
@@ -1,5 +1,5 @@
 const coreUrl = "https://osama-mart-server-backend.vercel.app/";
-// const coreUrl = "https://osama-mart-server-backend.vercel.app/";
+// const coreUrl = "http://localhost:7000/";
 
 export function baseUrl(route){
     return `${coreUrl}${route}` ;
