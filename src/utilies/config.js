fd23d5616ad8bab2eb50d4045b6cc535877720e6
tamzid620 <tamzid620@gmail.com>
2025-02-19 16:25:21 +0600

    config js coreURL changed

--- src/utilies/config.js
+++ src/utilies/config.js
@@ -1,5 +1,5 @@
-// const coreUrl = "https://osama-mart-server.onrender.com/";
-const coreUrl = "http://localhost:7000/";
+const coreUrl = "https://osama-mart-server.onrender.com/";
+// const coreUrl = "http://localhost:7000/";
 
 export function baseUrl(route){
     return `${coreUrl}${route}` ;
