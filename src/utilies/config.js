52ec40dc9d665b6c6d05357fe478892f2f9fb9d3
tamzid620 <tamzid620@gmail.com>
2025-06-28 18:42:23 +0600

    coreUrl changed 1

--- src/utilies/config.js
+++ src/utilies/config.js
@@ -1,6 +1,6 @@
 // const coreUrl = "https://osama-mart-server.onrender.com/";
-const coreUrl = "https://osama-mart-server.vercel.app/";
-// const coreUrl = "http://localhost:7000/";
+// const coreUrl = "https://osama-mart-server.vercel.app/";
+const coreUrl = "http://localhost:7000/";
 
 export function baseUrl(route){
     return `${coreUrl}${route}` ;
