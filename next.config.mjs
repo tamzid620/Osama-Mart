ef1e02a646c8e60e0d1af65443a6e0889f426bdc
tamzid620 <tamzid620@gmail.com>
2025-12-05 22:31:10 +0600

    config.js fixed

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,34 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      unoptimized: true, 
-        domains: [
-          'm.media-amazon.com' ,
-          'www.lego.com',
-          'cdn.shopify.com',
-          '1.bp.blogspot.com',
-          'i5.walmartimages.ca',
-          'cdn.ecommercedns.uk'
-        ],
-      }, 
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "m.media-amazon.com",
+      },
+      {
+        protocol: "https",
+        hostname: "www.lego.com",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.shopify.com",
+      },
+      {
+        protocol: "https",
+        hostname: "1.bp.blogspot.com",
+      },
+      {
+        protocol: "https",
+        hostname: "i5.walmartimages.ca",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.ecommercedns.uk",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
